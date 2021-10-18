d38560e0519baafc4df394c6d3cf4de841033558
Igor Decess <i.decess@comcast.net>
2021-10-17 21:09:17 -0600

    Work in progress on listTitles and searchShelf

--- src/shelf.js
+++ src/shelf.js
@@ -1,4 +1,3 @@
-
 function shelfBook(book, shelf) {
 
   if (shelf.length < 3) {
@@ -10,27 +9,35 @@ function unshelfBook(name, shelf) {
 
   for (i = 0; i < shelf.length; i++) {
     if (name === shelf[i].title) {
-    shelf.splice(i,1)
+      shelf.splice(i,1)
     }
   }
 }
-
 //////////////////////////////////////////////////////////
 function listTitles(fantasyShelf) {
 
-  //console.log(fantasyShelf[0].title);
-  //return fantasyShelf[0, 1, 2].title;
+  for (i = 0; i < fantasyShelf.length; i++) {
 
+    var titleList = fantasyShelf[i].title;
+  }
+  return (titleList);
 }
 /////////////////////////////////////////////////////////
+function searchShelf(name, shelf) {
 
+  for (i = 0; i < shelf.length; i++) {
+    if (name === shelf[i].title) {
 
+    } else {
 
+    }
+  }
+}
 
 /////////////////////////////////////////////////////////
 module.exports = {
   shelfBook,
   unshelfBook,
   listTitles,
-  // searchShelf
+  searchShelf
 };
