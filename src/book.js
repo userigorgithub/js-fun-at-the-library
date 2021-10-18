d2746d840ab4253ac1b9cdf7e44364f7d6819b21
Igor Decess <i.decess@comcast.net>
2021-10-17 20:31:38 -0600

    Refactor book.js (except editBook)

--- src/book.js
+++ src/book.js
@@ -1,24 +1,17 @@
 function createTitle(bookIdea) {
-  //var bookIdea = "Storm's Awakening";
-  //bookIdea = "The Storm's Awakening";
-  //var sushiTitle = "Dancing Sushi";
-  //sushiTitle = "The Dancing Sushi";
-  //var dragonTitle = "Dragon in the Summer";
-  //dragonTitle = "The Dragon in the Summer";
-  //var ghostTitle = "Teenage Ghoul";
-  //ghostTitle = "The Teenage Ghoul";
-  console.log(`The ${bookIdea}`);
+
   return `The ${bookIdea}`;
   //or return "The " + bookIdea;
 }
 //////////////////////////////////////////////////
 function buildMainCharacter(name, age, pronouns) {
+
   var myCharacter = {
     name: name,
     age: age,
     pronouns: pronouns,
   }
-  console.log(myCharacter);
+
   return myCharacter;
 }
 ///////////////////////////////////////////////////
@@ -31,32 +24,30 @@ function saveReview(addReviews, reviewsArray) {
     return addReviews;
   }
 }
-
 ///////////////////////////////////////////////////
 function calculatePageCount(bookTitle) {
 
   return bookTitle.length *20;
-
 }
 ////////////////////////////////////////////////////
 function writeBook(bookTitle, bookCharacter, genre) {
+
   var myBook = {
     title: bookTitle,
     mainCharacter: bookCharacter,
     pageCount: bookTitle.length *20,
     genre: "fantasy",
   };
+
   return myBook;
-
 }
-
 ////////////////////////////////////////////////////
 function editBook(bookTitle) {
-  var numberOfPages = bookTitle.pageCount;
-  console.log(numberOfPages * 0.75);
-  return numberOfPages * 0.75;
-}
 
+  console.log(bookTitle.pageCount * 0.75);
+  return bookTitle.pageCount * 0.75;
+}
+// Not 255 pages even though it logs in 255?
 ///////////////////////////////////////////////////
 module.exports = {
   createTitle,
