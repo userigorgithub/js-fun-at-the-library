3ed973e6e245f12310d8626441e855fd012447f5
Igor Decess <i.decess@comcast.net>
2021-10-17 21:48:36 -0600

    Add 3 tests for createLibrary

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,19 @@
+function createLibrary(libraryName, libraryShelves) {
 
+  var myLibrary = {
+    name: libraryName,
+    shelves: libraryShelves,
 
+  }
+
+  console.log(myLibrary);
+  return myLibrary;
+
+}
+
+/////////////////////////////////////////////////////
 module.exports = {
-  // createLibrary,
+  createLibrary,
   // addBook,
   // checkoutBook
 };
