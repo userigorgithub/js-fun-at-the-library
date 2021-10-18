4eccbd391e641456af31a71cd032b54000537811
Igor Decess <i.decess@comcast.net>
2021-10-17 19:58:42 -0600

    Add shelfBook and unshelfBook

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,36 @@
 
+function shelfBook(book, shelf) {
+
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+  }
+}
+//////////////////////////////////////////////////////////
+function unshelfBook(name, shelf) {
+
+  for (i = 0; i < shelf.length; i++) {
+    if (name === shelf[i].title) {
+    shelf.splice(i,1)
+    }
+  }
+}
+
+//////////////////////////////////////////////////////////
+function listTitles(fantasyShelf) {
+
+  //console.log(fantasyShelf[0].title);
+  //return fantasyShelf[0, 1, 2].title;
+
+}
+/////////////////////////////////////////////////////////
+
+
+
+
+/////////////////////////////////////////////////////////
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
+  shelfBook,
+  unshelfBook,
+  listTitles,
   // searchShelf
 };
