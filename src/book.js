35eb3b3684aed1c4bf9cc2746af07c0485742ebe
Igor Decess <i.decess@comcast.net>
2021-12-25 21:27:03 -0700

    Add book file with test file

--- src/book.js
+++ src/book.js
@@ -1,22 +1,17 @@
 function createTitle(bookIdea) {
-
   return `The ${bookIdea}`;
-  //or return "The " + bookIdea;
 }
-//////////////////////////////////////////////////
-function buildMainCharacter(name, age, pronouns) {
 
+function buildMainCharacter(name, age, pronouns) {
   var myCharacter = {
     name: name,
     age: age,
     pronouns: pronouns,
   }
-
   return myCharacter;
 }
-///////////////////////////////////////////////////
-function saveReview(addReviews, reviewsArray) {
 
+function saveReview(addReviews, reviewsArray) {
   if (reviewsArray.includes(addReviews)) {
     return reviewsArray;
   } else {
@@ -24,31 +19,25 @@ function saveReview(addReviews, reviewsArray) {
     return addReviews;
   }
 }
-///////////////////////////////////////////////////
+
 function calculatePageCount(bookTitle) {
-
-  return bookTitle.length *20;
+  return bookTitle.length * 20;
 }
-////////////////////////////////////////////////////
-function writeBook(bookTitle, bookCharacter, genre) {
 
+function writeBook(bookTitle, bookCharacter, genre) {
   var myBook = {
     title: bookTitle,
     mainCharacter: bookCharacter,
-    pageCount: bookTitle.length *20,
-    genre: "fantasy",
+    pageCount: bookTitle.length * 20,
+    genre: genre,
   };
-
   return myBook;
 }
-////////////////////////////////////////////////////
-function editBook(bookTitle) {
 
-  console.log(bookTitle.pageCount * 0.75);
-  return bookTitle.pageCount * 0.75;
+function editBook(myBook) {
+  return myBook.pageCount = myBook.pageCount *= 0.75;
 }
-// Not 255 pages even though it logs in 255?
-///////////////////////////////////////////////////
+
 module.exports = {
   createTitle,
   buildMainCharacter,
