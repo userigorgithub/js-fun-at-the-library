019f98474856004b1dd3a21c27ff85828de2d8ba
Igor Decess <i.decess@comcast.net>
2021-12-26 11:33:59 -0700

    Add library file and test file

--- src/Library.js
+++ src/Library.js
@@ -1,19 +1,40 @@
-function createLibrary(libraryName, libraryShelves) {
-
-  var myLibrary = {
-    name: libraryName,
-    shelves: libraryShelves,
-
+function createLibrary(name) {
+  var library = {
+    name: name,
+    shelves: shelf = {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    }
   }
-
-  console.log(myLibrary);
-  return myLibrary;
-
+  return library;
 }
 
-/////////////////////////////////////////////////////
+function addBook(library, bookName) {
+  if (bookName.genre === 'fantasy') {
+  library.shelves.fantasy.push(bookName);
+  }
+  if (bookName.genre === 'fiction') {
+  library.shelves.fiction.push(bookName);
+  }
+  if (bookName.genre === 'nonFiction') {
+  library.shelves.nonFiction.push(bookName);
+  }
+  return library;
+}
+
+function checkoutBook(library, bookName, genre) {
+  for (var i = 0; i < library.shelves[genre].length; i++) {
+    if (library.shelves[genre][i].title === bookName) {
+      library.shelves[genre].splice(i,1);
+      return `You have now checked out ${bookName} from the ${library.name}`;
+    }
+  }
+  return `Sorry, there are currently no copies of ${bookName} available at the ${library.name}`;
+}
+
 module.exports = {
   createLibrary,
-  // addBook,
-  // checkoutBook
+  addBook,
+  checkoutBook
 };
