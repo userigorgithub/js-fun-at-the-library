e7767af51d877d0445ac21c9ba7986fca4f2d123
Igor Decess <i.decess@comcast.net>
2021-12-25 21:15:19 -0700

    Update warm-up file

--- src/shelf.js
+++ src/shelf.js
@@ -1,35 +1,32 @@
 function shelfBook(book, shelf) {
-
   if (shelf.length < 3) {
     shelf.unshift(book);
   }
 }
-//////////////////////////////////////////////////////////
-function unshelfBook(name, shelf) {
 
+function unshelfBook(name, shelf) {
   for (i = 0; i < shelf.length; i++) {
     if (name === shelf[i].title) {
       shelf.splice(i,1)
     }
   }
 }
-//////////////////////////////////////////////////////////
-function listTitles(fantasyShelf) {
 
-  for (i = 0; i < fantasyShelf.length; i++) {
-
-    var titleList = fantasyShelf[i].title;
-  }
-  return (titleList);
+function listTitles() {
+//
+//   for (i = 0; i < fantasyShelf.length; i++) {
+//
+//     var titleList = fantasyShelf[i].title;
+//   }
+//   return (titleList);
 }
-/////////////////////////////////////////////////////////
-function searchShelf(name, shelf) {
 
+function searchShelf(name, shelf) {
   for (i = 0; i < shelf.length; i++) {
     if (name === shelf[i].title) {
-
+      return false;
     } else {
-
+      return true;
     }
   }
 }
