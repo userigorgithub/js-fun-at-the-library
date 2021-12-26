620889a03e5d6b3bf52c2f79df45c748c5dfe44a
Igor Decess <i.decess@comcast.net>
2021-12-25 22:14:06 -0700

    Add shelf file and test file

--- src/shelf.js
+++ src/shelf.js
@@ -5,33 +5,31 @@ function shelfBook(book, shelf) {
 }
 
 function unshelfBook(name, shelf) {
-  for (i = 0; i < shelf.length; i++) {
+  for (var i = 0; i < shelf.length; i++) {
     if (name === shelf[i].title) {
       shelf.splice(i,1)
     }
   }
 }
 
-function listTitles() {
-//
-//   for (i = 0; i < fantasyShelf.length; i++) {
-//
-//     var titleList = fantasyShelf[i].title;
-//   }
-//   return (titleList);
+function listTitles(titles) {
+  var titleList = [];
+  for (var i = 0; i < titles.length; i++) {
+    titleList.push(titles[i].title);
+  }
+  return titleList.join(', ');
 }
 
-function searchShelf(name, shelf) {
-  for (i = 0; i < shelf.length; i++) {
-    if (name === shelf[i].title) {
-      return false;
-    } else {
-      return true;
+function searchShelf(shelf, name) {
+  var onShelf = false;
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === name) {
+      onShelf = true;
     }
   }
+  return onShelf;
 }
 
-/////////////////////////////////////////////////////////
 module.exports = {
   shelfBook,
   unshelfBook,
