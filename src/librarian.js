7b57d75933479285fa90761a2285028293098871
Igor Decess <i.decess@comcast.net>
2022-01-21 20:37:22 -0700

    Add completed findBook and calculateLateFee methods

--- src/librarian.js
+++ src/librarian.js
@@ -1,6 +1,39 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+    this.isMorning = false;
+  }
+  greetPatron(patronName, isMorning) {
+    this.isMorning = true;
+    if (isMorning) {
+      return `Good morning, ${patronName}!`
+    }
+    return `Hello, ${patronName}!`;
+  }
+  findBook(book) {
+    for (var i = 0; i < this.library.shelves.fantasy.length; i++) {
+      if (this.library.shelves.fantasy[i].title === book) {
+        this.library.shelves.fantasy.splice(i, 1);
+        return `Yes, we have ${book}`;
+      }
+    }
+    for (var i = 0; i < this.library.shelves.nonFiction.length; i++) {
+      if (this.library.shelves.nonFiction[i].title === book) {
+        this.library.shelves.nonFiction.splice(i, 1);
+        return `Yes, we have ${book}`;
+      }
+    }
+    for (var i = 0; i < this.library.shelves.fiction.length; i++) {
+      if (this.library.shelves.fiction[i].title === book) {
+        this.library.shelves.fiction.splice(i, 1);
+        return `Yes, we have ${book}`;
+      }
+    }
+    return `Sorry, we do not have ${book}`;
+  }
+  calculateLateFee(fee) {
+    return Math.ceil(fee * 0.25);
   }
 }
 
